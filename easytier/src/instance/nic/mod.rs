9a0ff383e0a399957e84354874afd859bea6af75
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2026-03-29 18:46:44 +0200

    NicCtx

--- easytier/src/instance/nic/mod.rs
+++ easytier/src/instance/nic/mod.rs
@@ -37,204 +37,13 @@ use tokio::{
     task::JoinSet,
 };
 use tokio_util::bytes::Bytes;
-use tun::{AbstractDevice, AsyncDevice, Configuration, Layer};
+use ::tun::platform::Device;
+use ::tun::{AbstractDevice, AsyncDevice, Configuration, Layer};
 use zerocopy::{NativeEndian, NetworkEndian};
 
-pin_project! {
-    pub struct TunStream {
-        #[pin]
-        l: BiLock<AsyncDevice>,
-        cur_buf: BytesMut,
-        has_packet_info: bool,
-        payload_offset: usize,
-    }
-}
-
-impl TunStream {
-    pub fn new(l: BiLock<AsyncDevice>, has_packet_info: bool) -> Self {
-        let mut payload_offset = ZCPacketType::NIC.get_packet_offsets().payload_offset;
-        if has_packet_info {
-            payload_offset -= 4;
-        }
-        Self {
-            l,
-            cur_buf: BytesMut::new(),
-            has_packet_info,
-            payload_offset,
-        }
-    }
-}
-
-impl Stream for TunStream {
-    type Item = StreamItem;
-
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<StreamItem>> {
-        let self_mut = self.project();
-        let mut g = ready!(self_mut.l.poll_lock(cx));
-        reserve_buf(self_mut.cur_buf, 2500, 4 * 1024);
-        if self_mut.cur_buf.is_empty() {
-            unsafe {
-                self_mut.cur_buf.set_len(*self_mut.payload_offset);
-            }
-        }
-        let buf = self_mut.cur_buf.chunk_mut().as_mut_ptr();
-        let buf = unsafe { std::slice::from_raw_parts_mut(buf, 2500) };
-        let mut buf = ReadBuf::new(buf);
-
-        let ret = ready!(g.as_pin_mut().poll_read(cx, &mut buf));
-        let len = buf.filled().len();
-        if len == 0 {
-            return Poll::Ready(None);
-        }
-        unsafe { self_mut.cur_buf.advance_mut(len + TAIL_RESERVED_SIZE) };
-
-        let mut ret_buf = self_mut.cur_buf.split();
-        let cur_len = ret_buf.len();
-        ret_buf.truncate(cur_len - TAIL_RESERVED_SIZE);
-
-        match ret {
-            Ok(_) => Poll::Ready(Some(Ok(ZCPacket::new_from_buf(ret_buf, ZCPacketType::NIC)))),
-            Err(err) => {
-                log::error!("tun stream error: {:?}", err);
-                Poll::Ready(None)
-            }
-        }
-    }
-}
-
-#[derive(Debug, Clone, Copy, Default)]
-enum PacketProtocol {
-    #[default]
-    IPv4,
-    IPv6,
-    Other(u8),
-}
-
-// Note: the protocol in the packet information header is platform dependent.
-impl PacketProtocol {
-    #[cfg(any(target_os = "linux", target_os = "android", target_env = "ohos"))]
-    fn into_pi_field(self) -> Result<u16, io::Error> {
-        use nix::libc;
-        match self {
-            PacketProtocol::IPv4 => Ok(libc::ETH_P_IP as u16),
-            PacketProtocol::IPv6 => Ok(libc::ETH_P_IPV6 as u16),
-            PacketProtocol::Other(_) => Err(io::Error::other("neither an IPv4 nor IPv6 packet")),
-        }
-    }
-
-    #[cfg(any(target_os = "macos", target_os = "ios", target_os = "freebsd"))]
-    fn into_pi_field(self) -> Result<u16, io::Error> {
-        use nix::libc;
-        match self {
-            PacketProtocol::IPv4 => Ok(libc::PF_INET as u16),
-            PacketProtocol::IPv6 => Ok(libc::PF_INET6 as u16),
-            PacketProtocol::Other(_) => Err(io::Error::other("neither an IPv4 nor IPv6 packet")),
-        }
-    }
-
-    #[cfg(target_os = "windows")]
-    fn into_pi_field(self) -> Result<u16, io::Error> {
-        unimplemented!()
-    }
-}
-
-/// Infer the protocol based on the first nibble in the packet buffer.
-fn infer_proto(buf: &[u8]) -> PacketProtocol {
-    match buf[0] >> 4 {
-        4 => PacketProtocol::IPv4,
-        6 => PacketProtocol::IPv6,
-        p => PacketProtocol::Other(p),
-    }
-}
-
-struct TunZCPacketToBytes {
-    has_packet_info: bool,
-}
-
-impl TunZCPacketToBytes {
-    pub fn new(has_packet_info: bool) -> Self {
-        Self { has_packet_info }
-    }
-
-    pub fn fill_packet_info(
-        &self,
-        mut buf: &mut [u8],
-        proto: PacketProtocol,
-    ) -> Result<(), io::Error> {
-        // flags is always 0
-        buf.write_u16::<NativeEndian>(0)?;
-        // write the protocol as network byte order
-        buf.write_u16::<NetworkEndian>(proto.into_pi_field()?)?;
-        Ok(())
-    }
-}
-
-impl ZCPacketToBytes for TunZCPacketToBytes {
-    fn zcpacket_into_bytes(&self, zc_packet: ZCPacket) -> Result<Bytes, TunnelError> {
-        let payload_offset = zc_packet.payload_offset();
-        let mut inner = zc_packet.inner();
-        // we have peer manager header, so payload offset must larger than 4
-        assert!(payload_offset >= 4);
-
-        let ret = if self.has_packet_info {
-            let mut inner = inner.split_off(payload_offset - 4);
-            let proto = infer_proto(&inner[4..]);
-            self.fill_packet_info(&mut inner[0..4], proto)?;
-            inner
-        } else {
-            inner.split_off(payload_offset)
-        };
-
-        tracing::debug!(?ret, ?payload_offset, "convert zc packet to tun packet");
-
-        Ok(ret.into())
-    }
-}
-
-pin_project! {
-    pub struct TunAsyncWrite {
-        #[pin]
-        l: BiLock<AsyncDevice>,
-    }
-}
-
-impl AsyncWrite for TunAsyncWrite {
-    fn poll_write(
-        self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buf: &[u8],
-    ) -> Poll<Result<usize, io::Error>> {
-        let self_mut = self.project();
-        let mut g = ready!(self_mut.l.poll_lock(cx));
-        g.as_pin_mut().poll_write(cx, buf)
-    }
-
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), io::Error>> {
-        let self_mut = self.project();
-        let mut g = ready!(self_mut.l.poll_lock(cx));
-        g.as_pin_mut().poll_flush(cx)
-    }
-
-    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), io::Error>> {
-        let self_mut = self.project();
-        let mut g = ready!(self_mut.l.poll_lock(cx));
-        g.as_pin_mut().poll_shutdown(cx)
-    }
-
-    fn poll_write_vectored(
-        self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        bufs: &[io::IoSlice<'_>],
-    ) -> Poll<Result<usize, io::Error>> {
-        let self_mut = self.project();
-        let mut g = ready!(self_mut.l.poll_lock(cx));
-        g.as_pin_mut().poll_write_vectored(cx, bufs)
-    }
-
-    fn is_write_vectored(&self) -> bool {
-        true
-    }
-}
+mod platform;
+mod tun;
+use tun::{TunAsyncWrite, TunStream, TunZCPacketToBytes};
 
 pub struct VirtualNic {
     global_ctx: ArcGlobalCtx,
@@ -489,7 +298,7 @@ impl VirtualNic {
         Ok(())
     }
 
-    async fn create_tun(&self) -> Result<tun::platform::Device, Error> {
+    async fn create_tun(&self) -> Result<Device, Error> {
         let mut config = Configuration::default();
         config.layer(Layer::L3);
 
@@ -571,7 +380,7 @@ impl VirtualNic {
         config.up();
 
         let _g = self.global_ctx.net_ns.guard();
-        Ok(tun::create(&config)?)
+        Ok(::tun::create(&config)?)
     }
 
     #[cfg(any(
@@ -683,7 +492,7 @@ impl VirtualNic {
         let ft = TunnelWrapper::new(
             TunStream::new(a, has_packet_info),
             FramedWriter::new_with_converter(
-                TunAsyncWrite { l: b },
+                TunAsyncWrite::new(b),
                 TunZCPacketToBytes::new(has_packet_info),
             ),
             None,
@@ -1029,11 +838,7 @@ impl Nic {
         Ok(())
     }
 
-    #[cfg(any(
-        target_os = "android",
-        any(target_os = "ios", all(target_os = "macos", feature = "macos-ne")),
-        target_env = "ohos"
-    ))]
+    #[cfg(mobile)]
     pub async fn run_for_mobile(&mut self, tun_fd: std::os::fd::RawFd) -> Result<(), Error> {
         let tunnel = {
             let mut nic = self.nic.lock().await;
